3d1fc6fc88bf6472de8e4543856654255de63851
austintwang <austin.wang1357@gmail.com>
2019-02-23 16:09:43 -0500

    chipseq

--- run/make_jobs.py
+++ run/make_jobs.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
 	chr_dir = "/bcb/agusevlab/ashetty/stratas_prep_files/"
 	# chrs = ["KIRC.ALL.AS.chr{0}.vcf.gz".format(i + 1) for i in xrange(22)]
 	# chr_paths = [os.path.join(chr_dir, i) for i in chrs]
-	chr_info = {"all": "all_chrs.h3k27ac.vcf"}
+	chr_info = {"all": os.path.join(chr_dir, "all_chrs.h3k27ac.vcf")}
 
 	# Tumor Data
 	bed_path = "/bcb/agusevlab/ashetty/stratas_analysis_files/imbalanced_peaks.T.h3k27ac.101118.bed"

b1443a559bf6d2b288fe60f41d32205bf3dc8920
austintwang <austin.wang1357@gmail.com>
2019-02-24 20:03:06 -0500

    chipseq

--- run/make_ldsr_bed.py
+++ run/make_ldsr_bed.py
@@ -111,9 +111,23 @@ if __name__ == '__main__':
 
 	# make_bed(input_path, output_path, model_flavors)
 
-	# Kidney Data, Normal
-	input_path = "/bcb/agusevlab/awang/job_data/KIRC_RNASEQ/outs/1cv_normal_all"
-	output_path = "/bcb/agusevlab/awang/job_data/KIRC_RNASEQ/ldsr_beds/1cv_normal_all"
+	# # Kidney Data, Normal
+	# input_path = "/bcb/agusevlab/awang/job_data/KIRC_RNASEQ/outs/1cv_normal_all"
+	# output_path = "/bcb/agusevlab/awang/job_data/KIRC_RNASEQ/ldsr_beds/1cv_normal_all"
+	# model_flavors = "all"
+
+	# make_bed(input_path, output_path, model_flavors)
+
+	# Prostate Data, Tumor
+	input_path = "/bcb/agusevlab/awang/job_data/prostate_chipseq_tumor/outs/1cv_tumor_all"
+	output_path = "/bcb/agusevlab/awang/job_data/prostate_chipseq/ldsr_beds/1cv_tumor_all"
+	model_flavors = "all"
+
+	make_bed(input_path, output_path, model_flavors)
+
+	# Prostate Data, Normal
+	input_path = "/bcb/agusevlab/awang/job_data/prostate_chipseq_normal/outs/1cv_normal_all"
+	output_path = "/bcb/agusevlab/awang/job_data/prostate_chipseq/ldsr_beds/1cv_normal_all"
 	model_flavors = "all"
 
 	make_bed(input_path, output_path, model_flavors)

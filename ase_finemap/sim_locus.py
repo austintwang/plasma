856ab73f7cc6f772fa2f26317e3aea686424ca68
austintwang <austin.wang1357@gmail.com>
2019-06-18 17:49:58 -0400

    gwas simulation

--- ase_finemap/sim_locus.py
+++ ase_finemap/sim_locus.py
@@ -15,7 +15,8 @@ class LocusSimulator(object):
 		vcf_path, 
 		chrom, 
 		start, 
-		num_snps, 
+		num_snps,
+		num_causal, 
 		sample_filter=None,
 		maf_thresh=0.
 	):
@@ -73,6 +74,13 @@ class LocusSimulator(object):
 		self.snp_ids = np.array(snp_ids)
 		self.snp_count = snp_count
 
+		self.causal_inds = np.random.choice(self.snp_count, num_causal, replace=False)
+		self.num_causal = num_causal
+
+		haps_means = np.mean(self.haps, axis=0)
+		haps_centered = self.haps - haps_means
+		self.haps_cov = np.cov(haps_centered.T)
+
 	def sim_asqtl(
 			self, 
 			num_samples,
@@ -81,7 +89,7 @@ class LocusSimulator(object):
 			herit_qtl,
 			herit_as,
 			overdispersion,
-			num_causal
+			causal_override=None
 		):
 		haps_idx = np.random.choice(np.shape(self.haps)[0], num_samples, replace=False)
 		haps_sampled = self.haps[haps_idx]
@@ -91,12 +99,18 @@ class LocusSimulator(object):
 
 		genotypes_comb = hap_A + hap_B
 		phases = hap_A - hap_B
+		
+		if causal_override is not None:
+			causal_inds = causal_override
+			num_causal = np.size(causal_override)
+		else:
+			causal_inds = self.causal_inds
+			num_causal = self.num_causal
 
-		causal_effects = npr.normal(0, 1, num_causal)
-		causal_inds = npr.choice(self.num_snps, num_causal, replace=False)
-		causal_config = np.zeros(self.num_snps)
+		causal_effects = np.random.normal(0, 1, num_causal)
+		causal_config = np.zeros(self.snp_count)
 		np.put(causal_config, causal_inds, 1)
-		causal_snps = np.zeros(self.num_snps)
+		causal_snps = np.zeros(self.snp_count)
 		np.put(causal_snps, causal_inds, causal_effects)
 
 		prop_noise_eqtl = 1 - herit_qtl
@@ -139,115 +153,37 @@ class LocusSimulator(object):
 		counts_A[counts_A==0] = 1
 		counts_B[counts_B==0] = 1
 
-	def sim_gwas():
+		data_dict = {
+			"total_exp": total_exp,
+			"counts_A": counts_A,
+			"counts_B": counts_B,
+			"hap_A": hap_A,
+			"hap_B": hap_B,
+		}
 
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	data_path = os.path.join(dir_path, "haplotypes")
-	hap_name = "CEU.sampled_haplotypes"
-	hap_path = os.path.join(data_path, hap_name)
-	pickle_name = hap_name + ".pickle"
-	pickle_path = os.path.join(data_path, pickle_name)
-	num_haps = 190
+		return data_dict
 
-	haps = None
-	num_snps_total = None
+	def sim_gwas(
+			self, 
+			num_samples,
+			herit,
+			causal_override=None
+		):
+		if causal_override is not None:
+			causal_inds = causal_override
+			num_causal = np.size(causal_override)
+		else:
+			causal_inds = self.causal_inds
+			num_causal = self.num_causal
 
-	def __init__(self, params):
-		if Haplotypes.haps is None:
-			Haplotypes.load()
+		causal_effects = np.random.normal(0, 1, num_causal)
+		var_causal_raw = causal_effects.dot(self.haps_cov.dot(causal_effects))
+		scale = herit / var_causal_raw
+		causal_effects_scaled = causal_effects * np.sqrt(scale)
 
-		self.num_snps = params["num_snps"]
-		self.num_ppl = params["num_ppl"]
-		if self.num_ppl > Haplotypes.num_haps // 2:
-			raise ValueError("Not enough haplotypes to generate genotypes")
-			
-	@classmethod
-	def load(cls):
-		try:
-			with open(cls.pickle_path, "rb") as hapfile:
-				cls.haps = pickle.load(hapfile)
-		except StandardError:
-			# print("iosheiof") ####
-			cls.build()
-		finally:
-			# print(cls.haps) ####
-			cls.num_snps_total = cls.haps.shape[0]
+		signal = self.haps_cov * causal_effects_scaled
+		noise = np.random.multivariate_normal(0, self.haps_cov*(1-herit))
+		haps_var = np.diagonal(self.haps_cov)
+		z_scores = (signal + noise) * np.sqrt(self.snp_count / haps_var)
 
-	@classmethod
-	def build(cls):
-		# print("wheiieieiieiei") ####
-		cls.hap_files = os.listdir(cls.hap_path)
-		haps_list = [] 
-		for f in cls.hap_files:
-			# print("f") ####
-			if f.endswith(".haps"):
-				with open(os.path.join(cls.hap_path, f)) as hap:
-					hapstr = hap.read()
-				hap_block = [
-					[int(j) for j in i.strip().split("\t")] 
-					for i in hapstr.strip().split("\n")
-				]
-				for s in hap_block:
-					if all(0 <= i <= 1 for i in s):
-						prop_1 = sum(s) / len(s)
-						if 0.01 <= prop_1 <=0.99:
-							# print(len(s)) ####
-							# print(prop_1) ####
-							haps_list.append(s)
-		cls.haps = np.array(haps_list)
-		# print(cls.haps) ####
-		with open(cls.pickle_path, "wb") as hapfile:
-			pickle.dump(cls.haps, hapfile)
-		# print("wheifhwoeihfowe") ####
-		
-
-		# self.haps = {}
-		# self.hap_files = os.listdir(self.hap_path)
-		# # print(self.hap_files) ####
-		# for f in self.hap_files:
-		# 	if f.endswith(".haps"):
-		# 		with open(os.path.join(self.hap_path, f)) as hap:
-		# 			hapstr = hap.read()
-		# 		# print(hapstr) ####
-		# 		# print([[int(j) for j in i.strip().split("\t")] for i in hapstr.strip().split("\n")]) ####
-		# 		hap_arr = np.array(
-		# 			[[int(j) for j in i.strip().split("\t")] for i in hapstr.strip().split("\n")]
-		# 		).T
-		# 		# print(hap_arr) ####
-		# 		np.place(hap_arr, hap_arr>1, 0)
-		# 		if hap_arr.shape[1] == self.NUM_SNPS_RAW:
-		# 			self.haps[f] = hap_arr
-		# with open(self.pickle_path, "wb") as hapfile:
-		# 	pickle.dump(self.haps, hapfile)
-
-	def draw_haps(self):
-		start = np.random.randint(0, high=Haplotypes.num_snps_total-self.num_snps)
-		end = start + self.num_snps
-		section = np.arange(start, end)
-		haps_section = Haplotypes.haps[section]
-
-		locus_haps = haps_section.T
-
-		# num_ppl = int(self.NUM_HAPS / 2)
-		# locus = random.choice(list(self.haps))
-		# locus_haps = self.haps[locus]
-		a_ind = np.random.choice(Haplotypes.num_haps, self.num_ppl, replace=False)
-		a_set = set(a_ind)
-		b_ind = np.array([i for i in xrange(Haplotypes.num_haps) if i not in a_set])
-		hapA = locus_haps[a_ind]
-		hapB = locus_haps[b_ind]
-		np.random.shuffle(hapA)
-		np.random.shuffle(hapB)
-		# print(a_ind) ####
-		# print(b_ind) ####
-		# # print(locus_haps) ####
-		# haps_shuffled = np.random.shuffle(locus_haps)
-		# # print(haps_shuffled) ####
-		# hapA = haps_shuffled[:self.NUM_PPL]
-		# hapB = haps_shuffled[self.NUM_PPL:]
-		return hapA, hapB
-
-def sim_locus
-	vcf_reader = vcf.Reader(filename=chr_path)
-	ppl_names = vcf_reader.samples
-	num_ppl = len(ppl_names)
+		return z_scores

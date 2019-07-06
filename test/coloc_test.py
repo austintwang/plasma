7fb8b38bc7e55b21a162ac9d10e33f84f4dbfc75
austintwang <austin.wang1357@gmail.com>
2019-07-05 22:16:08 -0700

    debug gwas simulations

--- test/coloc_test.py
+++ test/coloc_test.py
@@ -263,6 +263,7 @@ def run_model(inputs, model_name, model_qtl_updates):
 	print(model_qtl.imbalance_stats) ####
 	print(model_qtl.total_exp_stats) ####
 	print(ppas_qtl) ####
+	print(inputs_gwas["z_gwas"]) ####
 	print(ppas_gwas) ####
 
 	result = {

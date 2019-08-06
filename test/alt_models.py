5e03b1c2c4fb94baf6d81f0a31dca650427c2c81
Austin Wang <austinwang@austins-mbp.dfci.harvard.edu>
2019-08-06 16:16:48 -0400

    simulations with rasqual and finemap

--- test/alt_models.py
+++ test/alt_models.py
@@ -444,6 +444,7 @@ class Rasqual(Finemap):
 			record = self.records_sim[snp]
 			sample = record.samples[smp]
 			gen_data = sample["GT"]
+			print(gen_data) ####
 			hap_data = gen_data.split("|")
 
 			reads = (0, 0)

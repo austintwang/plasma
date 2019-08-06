af5549ac921c55f86af120b083b8bb971c3c2df0
Austin Wang <austinwang@austins-mbp.dfci.harvard.edu>
2019-08-06 15:54:35 -0400

    simulations with rasqual and finemap

--- test/alt_models.py
+++ test/alt_models.py
@@ -46,6 +46,7 @@ class Caviar(Finemap):
 	def search_exhuastive(self, min_causal, max_causal):
 		self.min_causal = min_causal
 		self.max_causal = max_causal
+		print("testc") ####
 
 	def search_shotgun(self, min_causal, max_causal, *args):
 		self.search_exhuastive(min_causal, max_causal)

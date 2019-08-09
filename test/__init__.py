dad6f3d8d95f1e202ea1c42aaafdda2d5e4c3e4f
Austin Wang <austinwang@Austins-MBP>
2019-08-09 12:56:08 -0400

    simulations with rasqual and finemap

--- test/__init__.py
+++ test/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from plasma import Finemap, LocusSimulator
+from plasma import Finemap, LocusSimulator, Evaluator
 from .haplotypes import Haplotypes
 from .sim_ase import SimAse
 from .eval_caviar import EvalCaviar, EvalCaviarASE, EvalECaviar

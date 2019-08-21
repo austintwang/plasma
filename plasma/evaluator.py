8904af789dc06aa6c82f583b6492b3313a108297
Austin Wang <austinwang@Austins-MBP>
2019-08-21 14:35:52 -0700

    alternate greedy algorithm heuristic

--- plasma/evaluator.py
+++ plasma/evaluator.py
@@ -189,7 +189,7 @@ class Evaluator(object):
 			ppas_rev_sort = np.argsort(-ppas)
 			for i in ppas_rev_sort:
 				conf_sum_after = conf_sum - sum([probs[s] for s in snp_sets[i]])
-				print(conf_sum) ####
+				print(sum([probs[s] for s in snp_sets[i]])) ####
 				if conf_sum_after > confidence:
 					remove_set = snp_sets.pop(i)
 					for s in snp_sets.values():

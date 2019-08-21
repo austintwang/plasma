0c9b5370d92945de513b709107ab962b151c86fd
Austin Wang <austinwang@Austins-MBP>
2019-08-20 19:39:09 -0600

    test multiple causal variants

--- test/text_cent_bbox.py
+++ test/text_cent_bbox.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-from matplotlib.text.transforms import Affine2D, Bbox
+from matplotlib.transforms import Affine2D, Bbox
 
 class TextCentBbox(matplotlib.text.Text):
     def _get_layout(self, renderer):

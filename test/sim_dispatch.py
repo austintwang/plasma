dbc997c9b6d0e639b481789adff9c42d01f84aa0
Austin Wang <austinwang@Austins-MBP>
2019-08-26 18:32:40 -0400

    calibrate finemap

--- test/sim_dispatch.py
+++ test/sim_dispatch.py
@@ -470,7 +470,7 @@ if __name__ == '__main__':
 	curr_path = os.path.abspath(os.path.dirname(__file__))
 
 	script_path = os.path.join(curr_path, "sim_test.py")
-	batch_size = 5
+	batch_size = 11
 	num_trials = 500
 
 	disp = Dispatcher(script_path, batch_size)
@@ -554,7 +554,7 @@ if __name__ == '__main__':
 	# 	script_path,
 	# )
 
-	prior_stds = [0.00005, 0.0005, 0.005, 0.05, 0.5, 5, 50, 500]
+	prior_stds = [0.000005, 0.00005, 0.0005, 0.005, 0.05, 0.5, 5, 50, 500]
 	fmb_calib(
 		disp, 
 		data_info,

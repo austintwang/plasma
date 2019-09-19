650b3ff1ed71313a982f6728faba7f28688be7ea
Austin Wang <austinwang@Austins-MBP>
2019-09-18 21:29:29 -0400

    test cluster

--- test/diagnose_batch.py
+++ test/diagnose_batch.py
@@ -1,26 +1,35 @@
 import sys
 import os
 import subprocess
+import shutil
 
-def diagnose_sbatch(count, testfile, outdir):
-	if not os.path.exists(outdir):
-		os.makedirs(outdir)
-	for j in range(count):
-		job_args = [
-			"sbatch", 
-			"--export=ALL,PYTHONVERBOSE=2",
-			"--mem",
-			"10000",
-			"-o",
-			os.path.join(outdir, "{0:03d}.out".format(j)),
-			testfile,
-		]
-		submission = subprocess.run(job_args, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		print(str(submission.stdout, 'utf-8').rstrip())
+def diagnose_sbatch(count_per_node, nodelist, testfile, outdir):
+	if os.path.exists(outdir):
+		shutil.rmtree(output_path, ignore_errors=True)
+	os.makedirs(outdir)
+	for n in nodelist:
+		for j in range(count_per_node):
+			job_args = [
+				"sbatch", 
+				"-J",
+				"{0}_{1:02d}".format(n, j),
+				"-w",
+				"--export=ALL,PYTHONVERBOSE=1",
+				"--mem",
+				"10000",
+				"-o",
+				os.path.join(outdir, "{0}_{1:02d}.out".format(n, j)),
+				testfile,
+				n,
+				str(j)
+			]
+			submission = subprocess.run(job_args, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			print(str(submission.stdout, 'utf-8').rstrip())
 
 if __name__ == '__main__':
 	curr_path = os.path.abspath(os.path.dirname(__file__))
-	testfile = os.path.join(curr_path, "node_test.py")
+	testfile = os.path.join(curr_path, "node_test_2.py")
 	outdir = "/agusevlab/awang/batchtest"
-	diagnose_sbatch(100, testfile, outdir)
+	nodelist = ["node{0:02d}".format(i) for i in range(1, 20)]
+	diagnose_sbatch(10, nodelist, testfile, outdir)
 
---
+++ test/node_test_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print("PYTHON STARTED")
+
+import sys
+
+print("IMPORTS FINISHED")
+
+node = sys.argv[2]
+test_num = sys.argv[2]
+
+with open("/agusevlab/awang/batchtest/summary.txt", "a") as outfile:
+	outfile.write("NODE {0}, TEST {1:02d} COMPLETE\n".format(node, test_num))
+
+print("COMPLETE")

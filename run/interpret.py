e7731b8ab5df2fc3fc27fd1a187632f84c2ab6dc
Austin Wang <austinwang@Austins-MBP>
2019-09-25 10:03:08 -0400

    fresh multi cv

--- run/interpret.py
+++ run/interpret.py
@@ -450,11 +450,11 @@ def interpret(targets, target_dir, out_dir, name, model_flavors, thresholds, fai
 
 		successes += 1
 
-	# print(sorted(causal_zscores_fmb, key=np.size)[:20]) ####
-	# lst = sorted(causal_zscores_fmb, key=lambda x:x[0])[:20] ####
-	# for i in lst:
-	# 	print("\t".join([str(j) for j in i])) ####
-	# print("")
+	print(sorted(causal_zscores_fmb, key=np.size)[:20]) ####
+	lst = sorted(causal_zscores_fmb, key=lambda x:x[0])[:20] ####
+	for i in lst:
+		print("\t".join([str(j) for j in i])) ####
+	print("")
 
 	if fail_list_out is not None:
 		with open(fail_list_out, "wb") as fail_list_file:

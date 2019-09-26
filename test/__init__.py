01ce6cef8ac753a451686772ff4273a98a14d55d
Austin Wang <austinwang@Austins-MBP>
2019-09-26 12:54:57 -0400

    fresh multi cv

--- test/__init__.py
+++ test/__init__.py
@@ -6,6 +6,6 @@ from plasma import Finemap, LocusSimulator, Evaluator
 from .haplotypes import Haplotypes
 from .sim_ase import SimAse
 from .eval_caviar import EvalCaviar, EvalCaviarASE, EvalECaviar
-from .benchmark import Benchmark, Benchmark2d
+# from .benchmark import Benchmark, Benchmark2d
 from .alt_models import Caviar, CaviarASE, ECaviar, FmBenner, Rasqual
-from .text_cent_bbox import TextCentBbox
+# from .text_cent_bbox import TextCentBbox

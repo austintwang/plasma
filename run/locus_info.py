9f4368255deed8f752a8151ed66541bf5fbcc506
Austin Wang <austinwang@Austins-MBP>
2019-10-24 00:32:52 -0400

    loci for functional validation

--- run/locus_info.py
+++ run/locus_info.py
@@ -24,6 +24,8 @@ def write_loci(loci, loci_dir, out_dir, out_text_file):
         
         for j in np.nonzero(locus_out["causal_set_indep"])[0]:
             snp_id = locus_in["snp_ids"][j]
+            if snp_id is None:
+                print(locus_in["snp_ids"]) ####
             ppa = locus_out["ppas_indep"][j]
             # z_phi = locus_out["z_phi"][j]
             # z_beta = locus_out["z_beta"][j]

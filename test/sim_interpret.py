a2c3f0b1626769b82ec2e670e74f2ad565b5967b
Austin Wang <austinwang@Austins-MBP>
2019-10-31 23:57:48 -0400

    update graphics colors

--- test/sim_interpret.py
+++ test/sim_interpret.py
@@ -1370,15 +1370,15 @@ if __name__ == '__main__':
 	# 	res_dir_base
 	# )
 
-	std_al_dev = [0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
-	herit_as = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-	model_flavors = ["full", "indep", "ase", "acav"]
-	num_snps = 100
-	interpret_dev_herit(
-		data_dir_base, 
-		std_al_dev, 
-		herit_as,
-		model_flavors,
-		num_snps,
-		res_dir_base
-	)
+	# std_al_dev = [0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+	# herit_as = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+	# model_flavors = ["full", "indep", "ase", "acav"]
+	# num_snps = 100
+	# interpret_dev_herit(
+	# 	data_dir_base, 
+	# 	std_al_dev, 
+	# 	herit_as,
+	# 	model_flavors,
+	# 	num_snps,
+	# 	res_dir_base
+	# )

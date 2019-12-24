34f700f0a57ddd899d894845d93689e0ddc9b162
Austin Wang <austinwang@Austins-MBP>
2019-12-24 01:13:26 -0800

    colocalization ROC

--- test/coloc_interpret.py
+++ test/coloc_interpret.py
@@ -313,8 +313,8 @@ def calc_rocs(df_neg, df_pos, var_row, var_col, response):
                 (df_neg[var_row] == r) & (df_neg[var_col] == c),
                 [response]
             ].values.flatten()
-            print(ppt) ####
-            print(ppf) ####
+            # print(ppt) ####
+            # print(ppf) ####
 
             auroc = roc(ppt, ppf)
             struct.loc[r, c] = auroc
@@ -346,7 +346,7 @@ def interpret_corr(
     sns.set(font="Roboto")
 
     for m in model_flavors:
-        df_pos = df.loc[
+        df_pos = df_ctrl.loc[
             (df["model"] == m)
             & (df["herit_gwas"] == 0.001)
             & (df["complete"] == True)

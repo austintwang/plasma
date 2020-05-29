cef425ea4ebda335dcc7044ff4de1946bed9cc5c
Austin Wang <austinwang@Austins-MBP>
2020-05-29 14:23:07 -0700

    sample masks

--- plasma/finemap.py
+++ plasma/finemap.py
@@ -263,7 +263,7 @@ class Finemap(object):
         weights = 1 / self.imbalance_errors[self.mask_imbalance]
         denominator = 1 / (phases.T * weights * phases.T).sum(1) 
         self.phi = denominator * np.matmul(phases.T, (weights * self.imbalance[self.mask_imbalance])) 
-        print(self.imbalance[self.mask_imbalance]) ####
+        print(denominator) ####
 
     def _calc_imbalance_stats(self):
         """
